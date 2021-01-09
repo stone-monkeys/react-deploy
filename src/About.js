93784a3eec5419a5258afa8472e9aeb714cb60dc
Derry Bradley <derrybradley@live.ie>
2021-01-09 11:03:12 +0000

    adding adjustments to about text

--- src/About.js
+++ src/About.js
@@ -23,25 +23,30 @@ class About extends Component {
                         </p>
                             
                         <p id="pone"> 
-                            Here I developed a strong interest of the application of technology in the urban realm, 
+                            Here I developed a strong interest in the application of technology in the urban realm, 
                             in particular its use in solving human-centric problems. 
-                            My thesis sought to identify the extent in which digital platforms can be used in cities in regard to optimising the use of space by enabling the 
-                            efficient use of idle assets.
+                            My thesis sought to identify the extent in which digital platforms can be used in cities with regard to optimising the use of space, by enabling the 
+                            efficient use of idle assets.  I am very passionate about systems of interrelated devices that have the ability to transfer data over a network with no human intervention ie - IoT.
+                            I am also very interested in learning and applying how The Cloud can pave the way for this data to travel.
                         </p>
 
                         <p id="pone">
                             After graduating from university in 2018, I took a gap year and moved back to Toronto 
                             (I also studied there for a year in 2016/17). As planned, I then returned to Ireland to undertake a post-grad in 
-                            Software Development. Throughout this I got some work experience with a Tech-Ops team where
+                            Software Development. Throughout this I got some work with a Technical-Operations team where
                             developed a passion for the Cloud & the DevOps space.
                             I then joined Fidelity Investments as a Software Engineering Associate. 
-                            Here, I work with cutting edge technology on exciting projects 😄. Technolgies include Kubernetes & GoLang
+                          
+                        </p>
+
+                        <p id="pone">
+                            Here, I work with in a cloud computing unit with cutting edge technology on exciting projects 😄.
+                            Our aim is to build automated pipelines for a seamless integration with the cloud for our Devs.
+                            In doing so, we enable the Devs to focus above the value line (What we Deliver to our Customers) and 
+                            focus on the most complex aspects of the application
                         </p>
                         
-                        <p id="pone"> 
-                            I am very passionate about systems of interrelated devices that have the ability to transfer data over a network with no human intervention ie - IoT.
-                            I am also very interested in learning and applying how The Cloud can pave the way for this data to travel.
-                         </p>
+                       
                 </div>
 
     
